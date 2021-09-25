2831b9c8eaf2542ba9b40a26d0688080f9b5c402
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-09-25 14:44:18 +0700

    Create index.php

---
+++ src/rnd-content/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**************************************/
+/* Developed By : Priyankara Dilantha */
+/* Contact Me 	: www.dilantha.org ****/
+/**************************************/
+
+echo "Cannot run this without RND Engine.";
