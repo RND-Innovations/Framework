d39c646f6cf613ed91458e904b024843ecf5a965
DeveloperDilantha <74175124+DeveloperDilantha@users.noreply.github.com>
2021-09-25 14:32:04 +0700

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**************************************/
+/* Developed By : Priyankara Dilantha */
+/* Contact Me 	: www.dilantha.org ****/
+/**************************************/
+
+
+// Configuration here
+require_once( __DIR__ . "/config.php" );
+
+// Connect App Engine
+require_once( APP_ROOT . "/index.php");
